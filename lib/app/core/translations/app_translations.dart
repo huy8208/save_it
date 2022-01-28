5ef6c31d8a35e3f97a043645967f6ac4e0acbbd4
huy8208 <huy8208@gmail.com>
2022-01-27 16:11:52 -0800

    lowered SDK version and fixed import typo

--- lib/app/core/translations/app_translations.dart
+++ lib/app/core/translations/app_translations.dart
@@ -1,5 +1,5 @@
-import 'package:save_it/app/core/values/translations/en_US/en_us_translations.dart';
-import 'package:save_it/app/core/values/translations/vi_VN/vi_vn_translations.dart';
+import 'package:save_it/app/core/translations/en_US/en_us_translations.dart';
+import 'package:save_it/app/core/translations/vi_VN/vi_vn_translations.dart';
 
 abstract class AppTranslation {
   static Map<String, Map<String, String>> translations =
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:save_it/app/bindings/global_binding.dart';
 import 'package:save_it/app/core/theme/app_theme.dart';
-import 'package:save_it/app/core/values/translations/app_translations.dart';
+import 'package:save_it/app/core/translations/app_translations.dart';
 import 'package:save_it/app/routes/app_pages.dart';
 
 void main() {
