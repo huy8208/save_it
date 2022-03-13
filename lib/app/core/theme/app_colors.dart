d127906f4a8bcde680a74afa8df4eff467f6d7d5
huy8208 <huy8208@gmail.com>
2022-03-12 22:56:56 -0800

    UPDATE: Added more images for onboarding screens

--- lib/app/core/theme/app_colors.dart
+++ lib/app/core/theme/app_colors.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color.fromARGB(255, 191, 13, 207);
+  static const Color primaryColor = Color(0xFFBF0DCF);
   static const Color orange = Color(0xFFFB7026);
   static const Color lightOrange = Color(0xFFFC9158);
   static const Color darkOrange = Color(0xFFEA5404);
-  static const Color white = Colors.white;
+  static const Color canary = Color.fromRGBO(255, 255, 153, 1);
   static const Color black = Colors.black;
   static const Color bgColor = Color(0xFFF7F9FC);
   static const Color dark = Color(0xFF64748B);
   static const Color inviteButton = Color(0xFFFFF4EE);
-  static const Color violet = Color(0xFF9983E9);
   static const Color blue = Color(0xFF7891FC);
+  static const Color roseRed = Color(0xFFBF015C);
+  static const Color white = Colors.white;
 }
--- lib/app/core/theme/app_images_urls.dart
+++ lib/app/core/theme/app_images_urls.dart
@@ -1,4 +1,7 @@
 class AppImage {
   static const String logoImage = 'assets/images/logo.png';
   static const String allInOneFinance = 'assets/images/allInOne_finances.png';
+  static const String bankCentral = 'assets/images/bank-central.png';
+  static const String expenses = 'assets/images/expenses.png';
+  static const String assessAnywhere = 'assets/images/access-anywhere.png';
 }
