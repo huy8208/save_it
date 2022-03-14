6c7481d23be30f8df30233efd21978288223e929
huy8208 <huy8208@gmail.com>
2022-03-14 10:09:27 -0700

    Merged feature/authentication into develop branch

--- lib/app/modules/profile_screen/page.dart
+++ lib/app/modules/profile_screen/page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:save_it/app/core/theme/app_colors.dart';
 import 'package:save_it/app/core/theme/app_images_urls.dart';
 import 'package:save_it/app/core/theme/app_int.dart';
